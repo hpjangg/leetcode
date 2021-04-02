dc4641ae80fc0da0576147de713943bb9684297c
hpjangg <wkdghkvud3302@gmail.com>
2021-04-02 17:46:14 +0900

    BestTimeToBuyAndSellStock2

--- src/easy/interview/BestTimeToBuyAndSellStock2.java
+++ src/easy/interview/BestTimeToBuyAndSellStock2.java
@@ -13,7 +13,12 @@ class BestTimeToBuyAndSellStock2Imp {
     public int maxProfit(int[] prices) {
         int result = 0;
 
+        for (int i = 0; i < prices.length-1; i++) {
+            if (prices[i] < prices[i+1]) {
+                result += prices[i+1]-prices[i];
+            }
+        }
+
         return result;
     }
-
 }
