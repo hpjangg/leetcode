69d55892884d6cb2a0da096f93c0853144b51884
hpjangg <wkdghkvud3302@gmail.com>
2021-04-06 14:14:53 +0900

    ContainsDuplicate

---
+++ src/easy/interview/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+package easy.interview;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        new ContainsDuplicate().containsDuplicate(new int[]{1,2,3,1});
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num: nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
+        }
+
+        return false;
+    }
+
+}
