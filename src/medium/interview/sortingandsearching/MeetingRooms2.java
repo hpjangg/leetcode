903e997076e1a8ff34d05cb153eb974605eaa40c
hpjangg <wkdghkvud3302@gmail.com>
2021-06-19 00:43:08 +0900

    MeetingRooms2

---
+++ src/medium/interview/sortingandsearching/MeetingRooms2.java
@@ -0,0 +1,49 @@
+package medium.interview.sortingandsearching;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MeetingRooms2 {
+
+    public static void main(String[] args) {
+        new MeetingRooms2().minMeetingRooms(
+                new int[][]{{1, 10}, {2, 7}, {3, 19}, {8, 12}, {10, 20}, {11, 30}}
+        );
+    }
+
+    // Time Complexity: O(N log N)
+    // Space Complexity: O(N)
+    public int minMeetingRooms(int[][] intervals) {
+
+        // Check for the base case. If there are no intervals, return 0
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        // Min heap
+        PriorityQueue<Integer> allocator = new PriorityQueue<>(intervals.length, Comparator.comparingInt(a -> a));
+
+        // Sort the intervals by start time
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        // Add the first meeting
+        allocator.add(intervals[0][1]);
+
+        // Iterate over remaining intervals
+        for (int i = 1; i < intervals.length; i++) {
+
+            // If the room due to free up the earliest is free, assign that room to this meeting.
+            if (intervals[i][0] >= allocator.peek()) {
+                allocator.poll();
+            }
+
+            // If a new room is to be assigned, then also we add to the heap,
+            // If an old room is allocated, then also we have to add to the heap with updated end time.
+            allocator.add(intervals[i][1]);
+        }
+
+        // The size of the heap tells us the minimum rooms required for all the meetings.
+        return allocator.size();
+    }
+}
